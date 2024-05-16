2f5cf582a02c821e7f8019a3b201dc02f0bfe569
Sorin Giurgica <H3XM@it7.eurofins.com>
2024-05-16 17:48:48 +0200

    Initial commit

---
+++ Hospital.Repositories/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hospital.Repositories
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
